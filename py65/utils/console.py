d9916540e862a760b85dbf5eb100c210f17b74b2
Michael A. Morris <morrisma@mchsi.com>
2019-12-14 10:42:26 -0600

    Updated console to allow it to be used in Windows or Linux.

--- py65/utils/console.py
+++ py65/utils/console.py
@@ -1,80 +1,79 @@
 import sys
 
-# if sys.platform[:3] == "win":
-    # import msvcrt
+if sys.platform[:3] == "win":
+    import msvcrt
 
-    # def getch(stdin):
-        # """ Read one character from the Windows console, blocking until one
-        # is available.  Does not echo the character.  The stdin argument is
-        # for function signature compatibility and is ignored.
-        # """
-        # c = msvcrt.getch()
-        # if isinstance(c, bytes): # Python 3
-            # c = c.decode('Latin-1')
-        # return c
+    def getch(stdin):
+        """ Read one character from the Windows console, blocking until one
+        is available.  Does not echo the character.  The stdin argument is
+        for function signature compatibility and is ignored.
+        """
+        c = msvcrt.getch()
+        if isinstance(c, bytes): # Python 3
+            c = c.decode('Latin-1')
+        return c
 
-    # def getch_noblock(stdin):
-        # """ Read one character from the Windows console without blocking.
-        # Does not echo the character.  The stdin argument is for function
-        # signature compatibility and is ignored.  If no character is
-        # available, an empty string is returned.
-        # """
-        # if msvcrt.kbhit():
-            # return getch(stdin)
-        # return ''
+    def getch_noblock(stdin):
+        """ Read one character from the Windows console without blocking.
+        Does not echo the character.  The stdin argument is for function
+        signature compatibility and is ignored.  If no character is
+        available, an empty string is returned.
+        """
+        if msvcrt.kbhit():
+            return getch(stdin)
+        return ''
 
-# else:
-import select
-import os
-import termios
-import fcntl
+else:
+    import select
+    import os
+    import termios
+    import fcntl
 
-def getch(stdin):
-    """ Read one character from stdin, blocking until one is available.
-    Does not echo the character.
-    """
-    fd = stdin.fileno()
-    oldattr = termios.tcgetattr(fd)
-    newattr = oldattr[:]
-    newattr[3] &= ~termios.ICANON & ~termios.ECHO
-    try:
-        termios.tcsetattr(fd, termios.TCSANOW, newattr)
-        char = stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSAFLUSH, oldattr)
-    return char
-
-def getch_noblock(stdin):
-    """ Read one character from stdin without blocking.  Does not echo the
-    character.  If no character is available, an empty string is returned.
-    """
-
-    char = ''
-    fd = stdin.fileno()
-
-    try:
-        oldterm = termios.tcgetattr(fd)
-    except termios.error:  # https://github.com/mnaberez/py65/issues/46
+    def getch(stdin):
+        """ Read one character from stdin, blocking until one is available.
+        Does not echo the character.
+        """
+        fd = stdin.fileno()
+        oldattr = termios.tcgetattr(fd)
+        newattr = oldattr[:]
+        newattr[3] &= ~termios.ICANON & ~termios.ECHO
+        try:
+            termios.tcsetattr(fd, termios.TCSANOW, newattr)
+            char = stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldattr)
         return char
 
-    newattr = oldterm[:]
-    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    def getch_noblock(stdin):
+        """ Read one character from stdin without blocking.  Does not echo the
+        character.  If no character is available, an empty string is returned.
+        """
 
-    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+        char = ''
+        fd = stdin.fileno()
 
-    try:
-        r, w, e = select.select([fd], [], [], 0.1)
-        if r:
-            char = stdin.read(1)
-            if char == "\n":
-                char = "\r"
-    finally:
-        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
-    return char
+        try:
+            oldterm = termios.tcgetattr(fd)
+        except termios.error:  # https://github.com/mnaberez/py65/issues/46
+            return char
 
+        newattr = oldterm[:]
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+        try:
+            r, w, e = select.select([fd], [], [], 0.1)
+            if r:
+                char = stdin.read(1)
+                if char == "\n":
+                    char = "\r"
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+            fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+        return char
 
 def line_input(prompt='', stdin=sys.stdin, stdout=sys.stdout):
     """ Read a line from stdin, printing each character as it is typed.
